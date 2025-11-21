51999f153e7f6b1b62bc84177c0b1391677ed4c2
flamstert <flamstert@gmail.com>
2025-11-21 22:40:42 +0200

    remove reading role heroes everytime

--- dota_counter_finder.py
+++ dota_counter_finder.py
@@ -3,14 +3,13 @@ import counter_heroes
 import sys
 from prettytable import PrettyTable
 
-
 def main(hero_name, role):
     print(f"Best {role} counters for {hero_name}:")
-    most_picked_heroes_list = most_picked_heroes.get_heroes_data(role)
+    position_heroes_names = most_picked_heroes.get_position_heroes_names(role)
     counter_heroes_list = counter_heroes.get_counter_heroes_names(hero_name)
     table = PrettyTable(['Hero Name', 'Disadvantage', 'Counter\'s Win Rate', 'Matches Played'])
     for counter_hero in counter_heroes_list:
-        if(counter_hero.name in [hero.name for hero in most_picked_heroes_list] and counter_hero.disadvantage > 50):
+        if(counter_hero.name.lower() in position_heroes_names and counter_hero.disadvantage > 50):
             table.add_row([counter_hero.name, f"{counter_hero.disadvantage/100}%", f"{100-counter_hero.win_rate/100}%", counter_hero.matches_played])
     print(table)
 if __name__ == "__main__":
--- most_picked_heroes.py
+++ most_picked_heroes.py
@@ -1,17 +1,3 @@
-from bs4 import BeautifulSoup
-from curl_cffi import requests
-import time
-import random
-
-class Hero:
-    def __init__(self, name, win_rate, pick_rate):
-        self.name = name
-        self.win_rate = win_rate
-        self.pick_rate = pick_rate
-
-    def __str__(self):
-        return f"{self.name} - Win Rate: {self.win_rate/100}%, Pick Rate: {self.pick_rate/100}%"
-
 def get_role_name_encoded(name):
     match name.lower():
         case 'safe' | 'carry' | 'core':
@@ -30,20 +16,6 @@ def get_role_name_encoded(name):
             exit()
             return name
 
-def get_heroes_data(role):
-    time.sleep(random.uniform(3, 7))
-    url = f"https://www.dotabuff.com/heroes?show=heroes&view=winning&mode=all-pick&date=1m&rankTier=&position={get_role_name_encoded(role)}"
-
-    response = requests.get(url, impersonate='chrome110', timeout=10)
-    soup = BeautifulSoup(response.content, 'html.parser')
-
-    page = soup.find_all(class_='tw-border-b tw-transition-colors hover:tw-bg-muted/50 data-[state=selected]:tw-bg-muted')
-    heroes = []
-    for hero in page[1:]:
-        hero_name = hero.contents[0].text
-        hero_win_rate = int(hero.contents[1].text.replace('%', '').replace('.', ''))
-        hero_pick_rate = int(hero.contents[2].text.replace('%', '').replace('.', ''))
-        heroes.append(Hero(hero_name, hero_win_rate, hero_pick_rate))
-    
-    heroes.sort(key=lambda x: x.pick_rate, reverse=True)
-    return [i for i in heroes if i.pick_rate > 1000]
+def get_position_heroes_names(role):
+    with open(f'most-picked-heroes/{get_role_name_encoded(role)}.txt') as f:
+        return f.readline().split(';')
